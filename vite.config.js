d9ed55b3cf10452056dcc061096f1f5fe613505b
Javier Fray <fraydeveloper@gmail.com>
2026-01-04 21:50:28 -0500

    Configure custom server and preview ports; update `.env.example` with PORT variable

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,12 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    port: process.env.PORT || 5173,
+    host: true, // Listen on all addresses (0.0.0.0)
+  },
+  preview: {
+    port: process.env.PORT || 4173,
+    host: true, // Listen on all addresses (0.0.0.0)
+  },
 })
